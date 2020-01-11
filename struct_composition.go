4b8779d63b4df4fd05f3a567444429a050b8eebe
sudhakar-mnsr <sudhakar.mnsr@gmail.com>
2020-01-11 08:02:03 +0530

    composition

---
+++ struct_composition.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type fuel int
+const (
+   GASOLINE fuel = iota
+   BIO
+   ELECTRIC
+   JET
+)
+
+type vehicle struct {
+   make string
+   model string
+}
+
+type engine struct {
+   fuel fuel
+   thrust int
+}
+func (e *engine) start() {
+   fmt.Println("Engine started")
+}
+
+type truck struct {
+vehicle
+engine
+axels int
+wheels int
+class int
+}
+func (t *truck) drive() {
+   fmt.Printf("Truck %s %s, on the go!\n", t.make, t.model)
+}
+
+func main() {
+}
+
